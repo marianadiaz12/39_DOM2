a905152f9bca6aae44b6a6c86cba0b74eff088b1
marianadiaz12 <mariana.diazes@gmail.com>
2016-10-18 17:02:59 -0500

    Avance de DOM2

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,3 +1,18 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+var elNombre = document.getElementById("name").value;
+var elApellido = document.getElementById("lastname").value;
+// var elEmail = document.getElementById("input-email");
+var elPassword = document.getElementById("input-password").value;
+
+if(elNombre == null || elNombre.length == 0 && elApellido == null || elApellido == 0){
+	var errorNombre = document.createElement("span");
+	var textError = document.createTextNode("Se debe llenar este campo");
+	errorNombre.appendChild(textError);
+	document.getElementsByTagName('div')[1].appendChild(errorNombre);
+	document.getElementsByTagName('div')[2].appendChild(errorNombre);	
 }
+
+
+}
+
